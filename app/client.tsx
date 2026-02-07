56df09012e39b01e8ff692455d1355cb9ee48135
jealleal <ceoofgoosemail@gmail.com>
2026-02-07 17:41:21 +0300

    Update client.tsx

--- app/client.tsx
+++ app/client.tsx
@@ -170,7 +170,7 @@ export default function Home(props: {
                             <CurrentTime initial={props.timeServer} />
                         </p>
                     </div>
-                    <img alt="" src=https://lanyard.cnrad.dev/api/1158811379017449473?bg=0a0a0a&showDisplayName=true&hideActivity=true>
+                    <img alt="" src="https://lanyard.cnrad.dev/api/1158811379017449473?bg=0a0a0a&showDisplayName=true&hideActivity=true" />
                     <div className={styles.social}>
                         <a
                             href="https://github.com/jealleal"
