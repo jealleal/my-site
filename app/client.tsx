2cf057551df0223c2a756386b2165106bd1c6229
jealleal <ceoofgoosemail@gmail.com>
2026-02-07 19:13:18 +0300

    Update client.tsx

--- app/client.tsx
+++ app/client.tsx
@@ -176,7 +176,7 @@ export default function Home(props: {
                         <div className={styles.lanyardWrapper}>
                             <img 
                                 alt="Дискорд Профиль" 
-                                src="https://lanyard.cnrad.dev/api/1158811379017449473?bg=0a0a0a&showDisplayName=true&hideActivity=true"
+                                src="https://lanyard.cnrad.dev/api/1158811379017449473?showDisplayName=true&hideActivity=true"
                                 className={styles.profileCard}
                             />
                         </div>
